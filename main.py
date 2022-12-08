f53a68a6db0367b1876d5a49282ab2379c8ad6d8
dbw <dbw@dbwdeMacBook-Pro.local>
2022-12-09 02:03:22 +0800

    add XGBoost and Lightgbm

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import BaggingClassifier
 from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
 from sklearn.metrics import accuracy_score, roc_auc_score, f1_score
 
 def SVMModel():
@@ -22,11 +24,17 @@ def RandomSubspaceModel():
 def RandomForestModel():
 	return RandomForestClassifier(random_state=42, n_estimators=200)
 
+def XGBoostModel():
+	return XGBClassifier(random_state=42)
+
+def LightgbmModel():
+	return LGBMClassifier(random_state=42)
+
 def MetricFunc(label, pred):
 	return {'Accuracy': accuracy_score(label, pred), 'AUC': roc_auc_score(label, pred), 'F1 Score':f1_score(label, pred)}
 
 def SklearnMain(train_data, test_data):
-	ModelDict = {'SVM': SVMModel, 'DT': DecisionTreeModel, 'LR': LogisticRegressionModel, 'RS': RandomSubspaceModel, 'RF': RandomForestModel}
+	ModelDict = {'SVM': SVMModel, 'DT': DecisionTreeModel, 'LR': LogisticRegressionModel, 'RS': RandomSubspaceModel, 'RF': RandomForestModel, 'XGBoost': XGBoostModel, 'Lightgbm': LightgbmModel}
 	for model_name in ModelDict.keys():
 		model = ModelDict[model_name]()
 		model.fit(train_data['data'], train_data['label'])
