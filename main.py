330eaf14cba1aa8ee9f3e524c438811f7303ec25
董博文 <dbw22@mails.tsinghua.edu.cn>
2022-12-21 14:00:05 +0000

    KNN

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import BaggingClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
 from xgboost import XGBClassifier
 from lightgbm import LGBMClassifier
 from sklearn.metrics import accuracy_score, roc_auc_score, precision_score, recall_score, f1_score
@@ -30,11 +31,15 @@ def XGBoostModel():
 def LightgbmModel():
 	return LGBMClassifier(random_state=42)
 
+def KNearestNeighborsModel():
+	return KNeighborsClassifier()
+
 def MetricFunc(label, pred):
 	return {'Accuracy': accuracy_score(label, pred), 'AUC': roc_auc_score(label, pred), 'Precision':precision_score(label, pred), 'Recall':recall_score(label, pred), 'F1 Score':f1_score(label, pred)}
 
 def SklearnMain(train_data, test_data):
-	ModelDict = {'SVM': SVMModel, 'DT': DecisionTreeModel, 'LR': LogisticRegressionModel, 'RS': RandomSubspaceModel, 'RF': RandomForestModel, 'XGBoost': XGBoostModel, 'Lightgbm': LightgbmModel}
+	ModelDict = {'SVM': SVMModel, 'DT': DecisionTreeModel, 'LR': LogisticRegressionModel, 'RS': RandomSubspaceModel, 'RF': RandomForestModel, 'XGBoost': XGBoostModel, 'Lightgbm': LightgbmModel, 'KNN':KNearestNeighborsModel}
+	ModelDict = {'KNN':KNearestNeighborsModel}
 	for model_name in ModelDict.keys():
 		model = ModelDict[model_name]()
 		model.fit(train_data['data'], train_data['label'])
